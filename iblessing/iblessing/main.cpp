8742d8c0b99f2c2b6288bdd0f5878220137cf505
Soulghost <xiuyutong1994@163.com>
2020-07-21 20:49:50 +0800

    fix: typo

--- iblessing/iblessing/main.cpp
+++ iblessing/iblessing/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, const char *argv[]) {
            \n");
     
     // hello text
-    printf("[***] iblessing iOS Security Exploting Toolkit Beta 0.1 (http://blog.asm.im)\n");
+    printf("[***] iblessing iOS Security Exploiting Toolkit Beta 0.1 (http://blog.asm.im)\n");
     printf("[***] Author: Soulghost (高级页面仔) @ (https://github.com/Soulghost)\n\n");
     
     // parse args
