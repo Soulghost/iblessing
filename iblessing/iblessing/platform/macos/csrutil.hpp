fc9d0becebeee89384e866c5b0e01149df5b0bfe
Soulghost <xiuyutong1994@163.com>
2020-07-23 21:56:39 +0800

    feat: add SIP check

---
+++ iblessing/iblessing/platform/macos/csrutil.hpp
@@ -0,0 +1,23 @@
+//
+//  csrutil.hpp
+//  iblessing
+//
+//  Created by soulghost on 2020/7/23.
+//  Copyright © 2020 soulghost. All rights reserved.
+//
+
+#ifndef csrutil_hpp
+#define csrutil_hpp
+
+#include "Object.hpp"
+
+NS_IB_BEGIN
+
+class CSRUtil {
+public:
+    static bool isSIPon();
+};
+
+NS_IB_END
+
+#endif /* csrutil_hpp */
