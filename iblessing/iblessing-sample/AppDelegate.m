e6ca8eb065fbe1e4c61e9873a3232021acc1cad1
Soulghost <xiuyutong1994@163.com>
2020-07-26 12:45:41 +0800

    feat: add tests and samples

---
+++ iblessing/iblessing-sample/AppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.m
+//  iblessing-sample
+//
+//  Created by soulghost on 2020/7/25.
+//  Copyright © 2020 soulghost. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    return YES;
+}
+
+@end
