b3d8294b73a573d76f6d1139215b915bc54b720a
Soulghost <xiuyutong1994@163.com>
2020-08-13 19:54:44 +0800

    fix: deadlock in method signature compute

--- iblessing/iblessing/main.cpp
+++ iblessing/iblessing/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, const char *argv[]) {
            \n");
     
     // hello text
-    printf("[***] iblessing iOS Security Exploiting Toolkit Beta 0.2.2.1 (http://blog.asm.im)\n");
+    printf("[***] iblessing iOS Security Exploiting Toolkit Beta 0.2.3 (http://blog.asm.im)\n");
     printf("[***] Author: Soulghost (高级页面仔) @ (https://github.com/Soulghost)\n");
 
 #ifdef IB_CSR_ENABLED
