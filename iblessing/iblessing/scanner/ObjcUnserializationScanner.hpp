2abb571baa90b9c586ea990e0f0cda0f0949e97a
Soulghost <xiuyutong1994@163.com>
2020-08-16 21:29:00 +0800

    feat: add ObjcUnserializationScanner, not completed

---
+++ iblessing/iblessing/scanner/ObjcUnserializationScanner.hpp
@@ -0,0 +1,26 @@
+//
+//  ObjcUnserializationScanner.hpp
+//  iblessing
+//
+//  Created by Soulghost on 2020/8/16.
+//  Copyright © 2020 soulghost. All rights reserved.
+//
+
+#ifndef ObjcUnserializationScanner_hpp
+#define ObjcUnserializationScanner_hpp
+
+#include "Scanner.hpp"
+
+NS_IB_BEGIN
+
+class ObjcUnserializationScanner : public Scanner {
+public:
+    ObjcUnserializationScanner(std::string name, std::string desc): Scanner(name, desc) {}
+    
+    virtual ~ObjcUnserializationScanner() {};
+    virtual int start();
+};
+
+NS_IB_END
+
+#endif /* ObjcUnserializationScanner_hpp */
