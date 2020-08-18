8f1f18d190dccd667c3c6d44348a3e3433ee9371
Soulghost <xiuyutong1994@163.com>
2020-08-18 14:01:34 +0800

    fix: memory read problem for SEL in insecure unserialization scanner

---
+++ iblessing/iblessing-sample/insecureUnserialization/InsecureUnSerialization.h
@@ -0,0 +1,17 @@
+//
+//  InsecureUnSerialization.h
+//  iblessing-sample
+//
+//  Created by soulghost on 2020/8/18.
+//  Copyright © 2020 soulghost. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface InsecureUnSerialization : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
