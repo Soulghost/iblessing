8e35718fd25029c66438ca7adf29b95a391f7d8a
Soulghost <xiuyutong1994@163.com>
2020-09-25 18:39:30 +0800

    feat: add fat mach-o support for executable and dylib

--- iblessing/iblessing/scanner/context/ScannerContext.hpp
+++ iblessing/iblessing/scanner/context/ScannerContext.hpp
@@ -11,18 +11,32 @@
 
 #include "Object.hpp"
 #include "ScannerCommon.hpp"
+#include "ScannerWorkDirManager.hpp"
+#include "mach-universal.hpp"
+#include "mach-machine.h"
 #include <string>
 
 NS_IB_BEGIN
 
 class ScannerContext {
 public:
+    ScannerContext();
+    
     std::string getBinaryPath();
+    static scanner_err headerDetector(std::string binaryPath,
+                                      uint8_t **mappedFileOut,    /** OUT */
+                                      uint64_t *sizeOut,          /** OUT */
+                                      ib_mach_header_64 **hdrOut  /** OUT */);
+    static scanner_err headerDetector(uint8_t *mappedFile,        /** OUT */
+                                      ib_mach_header_64 **hdrOut, /** OUT */
+                                      uint64_t *archOffsetOut = nullptr, /** OUT */
+                                      uint64_t *archSizeOut = nullptr    /** OUT */);
     scanner_err archiveStaticLibraryAndRetry(std::string binaryPath);
     scanner_err setupWithBinaryPath(std::string binaryPath, bool reentry = false);
     
 private:
     std::string binaryPath;
+    std::shared_ptr<ScannerWorkDirManager> workDirManager;
 };
 
 NS_IB_END
---
+++ iblessing/iblessing/scanner/context/ScannerWorkDirManager.cpp
@@ -0,0 +1,71 @@
+//
+//  ScannerWorkDirManager.cpp
+//  iblessing
+//
+//  Created by soulghost on 2020/9/25.
+//  Copyright © 2020 soulghost. All rights reserved.
+//
+
+#include "ScannerWorkDirManager.hpp"
+#include "StringUtils.h"
+#include <filesystem>
+#include <sys/stat.h>
+
+using namespace std;
+using namespace iblessing;
+namespace fs = std::filesystem;
+
+ScannerWorkDirManager::ScannerWorkDirManager(string workDir) {
+    if (!StringUtils::has_prefix(workDir, "/tmp/")) {
+        workDir = "/tmp/";
+    }
+    
+    this->workDir = workDir;
+}
+
+string ScannerWorkDirManager::getWorkDir() {
+    return this->workDir;
+}
+
+int ScannerWorkDirManager::resetWorkDir() {
+    if (!cleanFolder() && !createWorkDirIfNeeded()) {
+        return 0;
+    }
+    return 1;
+}
+
+int ScannerWorkDirManager::createWorkDirIfNeeded() {
+    if (!fs::exists(workDir)) {
+        if (mkdir(workDir.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH ) != 0) {
+            return 1;
+        }
+        return 0;
+    }
+    return 0;
+}
+
+int ScannerWorkDirManager::cleanFolder() {
+    if (fs::exists(workDir)) {
+        if (fs::remove_all(workDir)) {
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int ScannerWorkDirManager::createShadowFile(std::string filePath, char **shadowPathOut /** OUT */) {
+    fs::path originPath = filePath;
+    
+    string fileName = originPath.filename();
+    fs::path shadowPath = StringUtils::path_join(workDir, fileName);
+    
+    try {
+        fs::copy(originPath, shadowPath);
+        *shadowPathOut = strdup(shadowPath.c_str());
+    } catch (fs::filesystem_error e) {
+        return 1;
+    }
+    return 0;
+}
