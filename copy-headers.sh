d7df9afd109b22ddd9091bddd9f6a8355390cbfe
Soulghost <xiuyutong1994@163.com>
2021-05-02 14:30:35 +0800

    x

--- copy-headers.sh
+++ copy-headers.sh
@@ -4,16 +4,11 @@ cd cmake-build
 true && rm -rf include
 mkdir include
 cd include
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    # copy iblessing core headers
-    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/v2/iblessing .
-    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/core ./iblessing
-    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/infra ./iblessing
-    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/common ./iblessing
-    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/scanner ./iblessing
-else
-    echo "not support!!!"
-fi
+rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/v2/iblessing .
+rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/core ./iblessing
+rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/infra ./iblessing
+rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/common ./iblessing
+rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/scanner ./iblessing
 
 # copy embed libs
 cd iblessing
