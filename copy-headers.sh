e973132457137fb5dd3ec6f3b87ef8df02a9b0d5
Soulghost <xiuyutong1994@163.com>
2021-05-02 13:58:03 +0800

    x

---
+++ copy-headers.sh
@@ -0,0 +1,26 @@
+set -xe
+
+cd cmake-build
+true && rm -rf include
+mkdir include
+cd include
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # copy iblessing core headers
+    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/v2/iblessing .
+    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/core ./iblessing
+    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/infra ./iblessing
+    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/common ./iblessing
+    rsync -a --prune-empty-dirs --include '*.h' --include '*.hpp' --include '*/' --exclude '*' ../../iblessing/iblessing/scanner ./iblessing
+else
+    echo "not support!!!"
+fi
+
+# copy embed libs
+cd iblessing
+mkdir vendor
+cp -R ../../../submodules/capstone/include/capstone ./vendor/
+cp -R ../../../submodules/keystone/include/keystone ./vendor/
+cp -R ../../../submodules/unicorn/include/unicorn ./vendor/
+
+cd ../../..
+pwd
