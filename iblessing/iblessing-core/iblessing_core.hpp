dd6c5d06f369f5d869c364547876e666ca07cc2b
Soulghost <xiuyutong1994@163.com>
2021-05-03 20:03:07 +0800

    x

---
+++ iblessing/iblessing-core/iblessing_core.hpp
@@ -0,0 +1,22 @@
+//
+//  iblessing_core.hpp
+//  iblessing-core
+//
+//  Created by Soulghost on 2021/5/3.
+//  Copyright © 2021 soulghost. All rights reserved.
+//
+
+#ifndef iblessing_core_
+#define iblessing_core_
+
+/* The classes below are exported */
+#pragma GCC visibility push(default)
+
+class iblessing_core
+{
+    public:
+    void HelloWorld(const char *);
+};
+
+#pragma GCC visibility pop
+#endif
---
+++ iblessing/iblessing-core/iblessing_corePriv.hpp
@@ -0,0 +1,18 @@
+//
+//  iblessing_corePriv.hpp
+//  iblessing-core
+//
+//  Created by Soulghost on 2021/5/3.
+//  Copyright © 2021 soulghost. All rights reserved.
+//
+
+/* The classes below are not exported */
+#pragma GCC visibility push(hidden)
+
+class iblessing_corePriv
+{
+    public:
+    void HelloWorldPriv(const char *);
+};
+
+#pragma GCC visibility pop
