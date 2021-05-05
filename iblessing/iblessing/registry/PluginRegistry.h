5615041fbf1f773b607de2420213acffc99b262f
Soulghost <xiuyutong1994@163.com>
2021-05-05 12:37:50 +0800

    feat: plugin preview

---
+++ iblessing/iblessing/registry/PluginRegistry.h
@@ -0,0 +1,22 @@
+//
+//  PluginRegistry.h
+//  iblessing
+//
+//  Created by Soulghost on 2021/5/5.
+//  Copyright © 2021 soulghost. All rights reserved.
+//
+
+#ifndef PluginRegistry_h
+#define PluginRegistry_h
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void registerPlugins(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* PluginRegistry_h */
