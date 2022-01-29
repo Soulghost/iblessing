e00201297c8f5f61d52f7c5d1de24f4f3d9dc81f
Soulghost <xiuyutong1994@163.com>
2022-01-29 16:28:22 +0800

    feat: misc

--- xpc/Swift-macOS-XPC-Example/StringManipulation/StringManipulation/main.m
+++ xpc/Swift-macOS-XPC-Example/StringManipulation/StringManipulation/main.m
@@ -1,6 +1,6 @@
 @import Foundation;
-@import XPCSupport;
-#import <StringManipulation-Swift.h>
+//@import XPCSupport;
+//#import <StringManipulation-Swift.h>
 
 int main(int argc, const char *argv[])
 {
@@ -12,7 +12,9 @@ int main(int argc, const char *argv[])
     xpc_connection_t listener = xpc_connection_create_mach_service("com.soulghost.dynamic.entrypoint", NULL, XPC_CONNECTION_MACH_SERVICE_LISTENER);
     xpc_connection_set_event_handler(listener, ^(xpc_object_t  _Nonnull peer) {
         xpc_connection_set_event_handler(peer, ^(xpc_object_t  _Nonnull object) {
-            xpc_connection_send_message(peer, object);
+            if (xpc_get_type(object) == XPC_TYPE_DICTIONARY) {
+                xpc_connection_send_message(peer, object);
+            }    
         });
         xpc_connection_resume(peer);
     });
